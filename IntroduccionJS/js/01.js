ff11348d4064eb19c179f0b2e2447e1b56214f98
seba-m <sebaah89@gmail.com>
2021-07-18 14:56:01 -0400

    agregado proyecto para ir iniciando en js.

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,25 @@
+//variables
+
+var producto = 'Audifonos Gamer'; //inicializamos la variable y asignamos el valor.
+
+var disponible; //inicializamos la variable, pero sin el valor.
+
+//producto = true; //Reasignando el valor de la variable.
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//var 1disponible1;
+var disponible_;
+
+//estilos para las variables
+var nombre_producto = 'Monitor HD'; //underscore
+var nombreProducto = 'Monitor HD'; //camelcase
+var NombreProducto = 'Monitor HD'; //pascalcase
+var nombreproducto = 'Monitor HD'; //lowercase
+
+
+console.log(producto);
